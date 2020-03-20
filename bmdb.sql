1950caf85c1b484e1302a2f79936965f297400b4
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-03-19 21:59:55 -0400

    Update bmdb.sql

--- bmdb.sql
+++ bmdb.sql
@@ -6,31 +6,22 @@ use bmdb;
 
 -- DDL - Create Tables
 create table movie (
-	id 				int 			primary key auto_increment,
-	ssnLastFour 	int 			not null,
-	firstName 		varchar(25) 	not null,
-	lastName 		varchar(25) 	not null,
-	middleInitial 	varchar(1), 		
-	address			varchar(50) 	not null,
-	city 			varchar(40) 	not null,
-	state 			varchar(2) 		not null,
-	zip 			varchar(5) 		not null,
-	birthday 		Date 			not null,
-	primaryPhone 	varchar(12) 	not null,
-	secondaryPhone 	varchar(12),
-	email 			varchar(50) 	not null
+	id 				int 			primary key,
+	Title			varchar(100) 	not null,
+	year			int 			not null,
+	rating			varchar(5) 		not null,		
+	Director		varchar(50) 	not null
 	);
 
 create table credit (
-	id 				int 			not null primary key auto_increment,
-	subject 		varchar(50)		not null,
-	name 			varchar(20)		not null unique,
-	credits 		int 			not null,
-	quarter 		int 			not null
+	id 				int 			not null auto_increment,
+	actorID			int				not null,
+	movieID 		int				primary key,
+	role 			varchar(50)		not null
 	);
 
 
-create table enrolled (
+create table actor (
 	id 				int 			not null primary key auto_increment,
 	studentID		int 			not null,
 	courseID		int 			not null,
